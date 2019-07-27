dfa6478dd1c05c5d7e742bccd691f5a0e57ee34e
foxraya <53102693+foxraya@users.noreply.github.com>
2019-07-27 18:04:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HandleLatest
+{
+    class Program
+    {
+        void HandleInput(String fileName)
+        {
+            using (var fileStream = File.OpenRead(fileName))
+            {
+                using (var streamReader = new StreamReader(fileStream, 128))
+                {
+                    String line;
+                    String current;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        if (float.TryParse(line, out price))
+                        {
+                            current = line;
+                        }
+                        else
+                        {
+                            if (!current.IsNullOrEmpty())
+                                Console.WriteLine(current);
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var fileName = "test.txt";
+            HandleLatest(fileName);
+        }
+    }
+}
